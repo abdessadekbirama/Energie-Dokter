79c756cb21e65e1d0aad89a01da319e02eb61f0a
abdessadek <abdessadekbirama1@gmail.com>
2025-11-21 20:46:30 +0100

    test

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,6 +25,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link
           href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700;800;900&display=swap"
           rel="stylesheet"
